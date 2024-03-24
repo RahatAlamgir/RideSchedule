9ce9803a36dc7720f0919ee834bf54e67b6b9df2
Rahat Alamgir <158708641+RahatAlamgir@users.noreply.github.com>
2024-03-24 17:45:39 +0600

    fix the model
    i thing the problem is in PK and FK

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import user
+from .models import user , Rider,Driver,Schedule
 # Register your models here.
-admin.site.register([user])
+admin.site.register([user,Rider,Driver,Schedule])
--- main/models.py
+++ main/models.py
@@ -3,9 +3,30 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class user(models.Model):
+class user(models.Model ):
+    id = models.CharField(max_length=10, primary_key=True,blank=False,null=False)
     name = models.CharField(max_length = 200)
     state = models.CharField(max_length = 10)
     postal_Code = models.CharField(max_length = 10)
     country = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=11)
+
+class Rider(models.Model):
+    rider_id = models.CharField(max_length = 10,primary_key=True,blank=False,null=False)
+    id = models.ForeignKey(user,on_delete=models.CASCADE,blank=True,null=True)
+class Driver(models.Model):
+    driver_id = models.CharField(max_length=10,primary_key=True,blank=False,null=False)
+    id = models.ForeignKey(user, on_delete=models.CASCADE,blank=True,null=True)
+class Schedule(models.Model):
+    schedule_id = models.CharField(max_length=10)
+    rider_id = models.ForeignKey(Rider,on_delete=models.CASCADE,blank=True,null=True)
+    driver_id = models.ForeignKey(Driver,on_delete=models.CASCADE,blank=True,null=True)
+    pickUp_time = models.CharField(max_length=10)
+    pickup_from = models.CharField(max_length=50)
+    drop_to = models.CharField(max_length=50)
+    schedule_type = (
+        ('daily','daily'),
+        ('weekly', 'weekly'),
+        ('monthly', 'monthly'),
+        ('custom', 'custom'))
+    type_of_schedule = models.CharField(max_length=10,choices=schedule_type,blank=True,null=True)
