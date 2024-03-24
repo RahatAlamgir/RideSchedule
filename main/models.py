28a9131f2cdbf616fba870492ef3055db8ff0575
Rahat Alamgir <158708641+RahatAlamgir@users.noreply.github.com>
2024-03-24 16:57:36 +0600

    models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class user(models.Model):
+    name = models.CharField(max_length = 200)
+    state = models.CharField(max_length = 10)
+    postal_Code = models.CharField(max_length = 10)
+    country = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=11)
