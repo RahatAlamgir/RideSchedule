d7c029989452b3fbe2b2fc1494d66ff619509259
Ashrafi Siddika <103596785+ashrafisiddika@users.noreply.github.com>
2024-03-25 00:18:27 +0600

    modelDesign

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class user(models.Model ):
-    id = models.CharField(max_length=10, primary_key=True,blank=False,null=False)
+    id = models.CharField(max_length=10, primary_key=True, blank=False, null=False)
     name = models.CharField(max_length = 200)
     state = models.CharField(max_length = 10)
     postal_Code = models.CharField(max_length = 10)
@@ -12,16 +12,16 @@ class user(models.Model ):
     phone_number = models.CharField(max_length=11)
 
 class Rider(models.Model):
-    rider_id = models.CharField(max_length = 10,primary_key=True,blank=False,null=False)
-    id = models.ForeignKey(user,on_delete=models.CASCADE,blank=True,null=True)
+    rider_id = models.CharField(max_length = 10, primary_key=True, blank=False, null=False)
+    id = models.ForeignKey(user, on_delete=models.CASCADE, blank=True, null=True)
 class Driver(models.Model):
-    driver_id = models.CharField(max_length=10,primary_key=True,blank=False,null=False)
+    driver_id = models.CharField(max_length=10, primary_key=True, blank=False, null=False)
     id = models.ForeignKey(user, on_delete=models.CASCADE,blank=True,null=True)
 class Schedule(models.Model):
     schedule_id = models.CharField(max_length=10)
-    rider_id = models.ForeignKey(Rider,on_delete=models.CASCADE,blank=True,null=True)
-    driver_id = models.ForeignKey(Driver,on_delete=models.CASCADE,blank=True,null=True)
-    pickUp_time = models.CharField(max_length=10)
+    rider_id1 = models.ForeignKey(Rider, on_delete=models.CASCADE, blank=True, null=True)
+    driver_id1 = models.ForeignKey(Driver, on_delete=models.CASCADE, blank=True, null=True)
+    pickUp_time = models.TimeField(max_length=10)
     pickup_from = models.CharField(max_length=50)
     drop_to = models.CharField(max_length=50)
     schedule_type = (
@@ -29,4 +29,4 @@ class Schedule(models.Model):
         ('weekly', 'weekly'),
         ('monthly', 'monthly'),
         ('custom', 'custom'))
-    type_of_schedule = models.CharField(max_length=10,choices=schedule_type,blank=True,null=True)
+    type_of_schedule = models.CharField(max_length=10, choices=schedule_type, blank=True, null=True)
