b475d9d7db1081afa8f706b6f52c1bac5e2f2d35
Rahat Alamgir <158708641+RahatAlamgir@users.noreply.github.com>
2024-05-11 21:06:22 +0600

    IDK

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     
     phone = models.CharField(max_length=20,null=True,blank=True)
     address = models.CharField(max_length=50,null=True, blank=True)
